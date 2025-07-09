428fd34a25de4bbcf72865310e001dc42979b2bd
Vijaya Lakshmi <vijayalakshmi651@gmail.com>
2025-07-09 17:48:03 +0530

    added space in strings

--- strings/atoi.c
+++ strings/atoi.c
@@ -6,6 +6,7 @@ C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
+
 #include <stdio.h>
 
 int main()
